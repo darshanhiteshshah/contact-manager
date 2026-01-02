82e2cd7c37b61b1e5ba7bec19b40e3b78fb1cc19
darshan <darshanhiteshshah@gmail.com>
2026-01-02 18:43:59 +0530

    new front

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://contact-mmanager.vercel.app'],
+  origin: ['http://localhost:5173', 'https://contact-manager-darshan.vercel.app'],
   credentials: true
 }));
 app.use(express.json());
